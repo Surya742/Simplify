284e91b7256bceed5c81acdb2f302d566d638549
Surya742 <suryakumarsahani.742@gmail.com>
2022-05-29 01:34:35 +0530

    build pdf viewer

--- src/App.js
+++ src/App.js
@@ -1,23 +1,56 @@
-import logo from './logo.svg';
+import React, { useRef, useEffect} from 'react';
+import WebViewer from '@pdftron/pdfjs-express';
+import { styled } from '@mui/material/styles';
+import Button from '@mui/material/Button';
+import IconButton from '@mui/material/IconButton';
+import PhotoCamera from '@mui/icons-material/PhotoCamera';
+import Stack from '@mui/material/Stack';
 import './App.css';
 
+const Input = styled('input')({
+  display: 'none',
+});
+
 function App() {
+  const viewer = useRef(null);
+  const input = document.getElementById('file_upload');
+  input.addEventListener('change', () => {
+    console.log('1')
+  });
+
+  useEffect(() => {
+    WebViewer(
+      {
+        path: 'lib',
+        initialDoc: 'test.pdf',
+      },
+      viewer.current,
+    ).then(instance => {
+      input.addEventListener('change', () => {
+        // Get the file from the input
+        const file = input.files[0];
+        console.log('2')
+        instance.UI.loadDocument(file, { filename: file.name });
+      });
+
+      const { documentViewer } = instance.Core;
+      documentViewer.addEventListener('documentLoaded', () => {
+        // perform document operations
+        console.log('loaded')
+      });
+    });
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="MyComponent">
+      <label htmlFor="file_upload">
+        <Input accept=".pdf" id="file_upload" name="file_upload" type="file" />
+        {/* <Button variant="contained" component="span">
+          Upload
+        </Button> */}
+      </label>
+      <div className="header">Simplify</div>
+      <div className="webviewer" ref={viewer}></div>
     </div>
   );
 }
